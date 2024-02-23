647efbcf35134f68c65cde3fc3ae3e6e2a58944a
Renzo Viscio <renzo@gecco.com.mx>
2024-02-23 18:58:18 -0300

    Chapter 6

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,5 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
- 
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
@@ -9,4 +9,4 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
   );
-}   
+}
